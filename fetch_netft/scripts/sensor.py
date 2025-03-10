24df195d251190b67ca4ce4148004406765ba245
Felipe <lfcasas7@hotmail.com>
2025-03-10 14:53:28 -0500

    New external force topic

--- fetch_netft/scripts/sensor.py
+++ fetch_netft/scripts/sensor.py
@@ -71,8 +71,8 @@ class Sensor:
             
             if self.robot_type not in config:
                 rospy.logerr("Robot type '%s' not found in %s. Using defaults." % (self.robot_type, yaml_path))
-                self.gripper_mass = 1.5
-                self.gripper_cog = np.array([0.5, 0.0, 0.0])
+                self.gripper_mass = 1.56
+                self.gripper_cog = np.array([0.0547, 0.0, 0.0])
             else:
                 robot_config = config[self.robot_type]
                 self.gripper_mass = float(robot_config['gripper_mass'])  # Ensure float
@@ -190,7 +190,7 @@ class Sensor:
             (trans, rot) = self.tf_listener.lookupTransform("base_link", "ati_link", rospy.Time(0))
             R = quaternion_matrix(rot)[:3, :3]
             gravity_base = np.array([0.0, 0.0, -self.gravity])
-            F_gravity = self.gripper_mass * R.dot(gravity_base)
+            F_gravity = self.gripper_mass * R.T.dot(gravity_base)
             T_gravity = np.cross(self.gripper_cog, F_gravity)
 
             external_msg = WrenchStamped()
